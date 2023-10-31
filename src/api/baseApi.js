7207f86a8c53034e27cd32b5bb106f6acd782c7e
anirudh2608 <anirudhsinghh26@gmail.com>
2023-10-31 16:16:23 +0530

    modified API

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -5,10 +5,10 @@ const baseAPI = axios.create({
     params: {
 
         // cristianoanirudh26@gmail.com
-        key: "AIzaSyAmhdOORZBcNEQXXXBarhfhvE5n9KwWl0U"
+        // key: "AIzaSyAmhdOORZBcNEQXXXBarhfhvE5n9KwWl0U"
 
         // anirudhsinghh26@gmail.com
-        // key: "AIzaSyD8foCMuMW4U6Xc7xD4L9H8AKt0TJxk-WY"
+        key: "AIzaSyD8foCMuMW4U6Xc7xD4L9H8AKt0TJxk-WY"
     },
     headers: {
         Accept: 'application/json',
